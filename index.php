4cbcc3929ad5053de9ac90268beede52a0c1a5a3
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-11-25 18:29:56 +0700

    fixing image in template

--- index.php
+++ index.php
@@ -53,11 +53,10 @@
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col col-xl-10">
-                    <div class="card" style="border-radius: 1rem;">
+                    <div class="card">
                         <div class="row g-0">
                             <div class="col-md-6 col-lg-5 d-none d-md-block">
-                                <img src="./assets/img/clean2.jpg" alt="login image" class="img-fluid"
-                                    style="border-radius: 1rem 0 0 1rem;" />
+                                <img src="./assets/img/clean2.jpg" alt="login image" class="img-fluid h-100 img-fill"/>
                             </div>
                             <div class="col-md-6 col-lg-7 d-flex align-items-center">
                                 <div class="card-body p-4 p-lg-5 text-black">
@@ -66,7 +65,7 @@
                                         <span class="h1 fw-bold mb-0">Account</span>
                                     </div>
 
-                                    <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Your account information</h5>
+                                    <h5 class="fw-normal mb-3 pb-3">Your account information</h5>
                                     <form>
                                         <div class="form-floating mb-4">
                                             <input type="text" name="username" id="username"
