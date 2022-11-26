6706dfab2b1e1f3d714acd6536dfbc9fd49ffce0
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-11-26 16:35:47 +0700

    add navbar

--- navbar.php
+++ navbar.php
@@ -1,61 +1,27 @@
-<header class="mb-5">
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
-        <div class="container">
-            <a class="navbar-brand" href="#!"><span style="color: #5e9693;">Psycho</span><span
-                    style="color: #fff;">logist</span></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <i class="fas fa-bars"></i>
-            </button>
-            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button> -->
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item custom-nav-item">
-                        <a class="nav-link text-white" href="#!">Login</a>
-                    </li>
-                    <li class="nav-item custom-nav-item">
-                        <a class="nav-link text-white" href="#!">Sign Up</a>
-                    </li>
-                    <li class="nav-item custom-nav-item">
-                        <a class="nav-link text-white" href="#!">Profile</a>
-                    </li>
-                    <!-- <li class="nav-item">
-                        <a class="nav-link" href="#!">Reference</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#!">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#!">Team</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#!">Contact</a>
-                    </li> -->
-                </ul>
-                <!-- <ul class="navbar-nav d-flex flex-row">
-                    <li class="nav-item me-3 me-lg-0">
-                        <a class="nav-link" href="#!">
-                            <i class="fas fa-shopping-cart"></i>
-                        </a>
-                    </li>
-                    <li class="nav-item me-3 me-lg-0">
-                        <a class="nav-link" href="#!">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                    </li>
-                    <li class="nav-item me-3 me-lg-0">
-                        <a class="nav-link" href="#!">
-                            <i class="fab fa-instagram"></i>
-                        </a>
-                    </li>
-                </ul> -->
-            </div>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
+    <div class="container">
+        <a href="#" class="navbar-brand">
+            <img src="./assets/img/jawa-power.png" alt="Jawa jawa-power" class="img-fluid h-75 w-75">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ">
+                <li class="nav-item custom-nav-item align-item-center align-self-center">
+                    <a class="nav-link text-dark-custom" href="./login.php">Login</a>
+                </li>
+                <li class="nav-item custom-nav-item align-item-center align-self-center">
+                    <a class="nav-link text-dark-custom" href="./register.php">Sign Up</a>
+                </li>
+                <li class="nav-item custom-nav-item align-item-center align-self-center">
+                    <a class="nav-link text-dark-custom" href="./">Profile</a>
+                </li>
+                
+            </ul>
         </div>
-    </nav>
-</header>
+    </div>
+</nav>
