e30190d8f230dce3e636247db9d32f5e68450cb2
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-11-27 14:58:50 +0700

    fixing navbar

--- navbar.php
+++ navbar.php
@@ -1,14 +1,23 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
     <div class="container">
-        <a href="#" class="navbar-brand">
-            <img src="./assets/img/jawa-power.png" alt="Jawa jawa-power" class="img-fluid h-75 w-75">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation">
+        <!-- <a href="#" class="navbar-brand">
+            <img src="./assets/img/jawa-power.png" alt="Jawa jawa-power" class="img-fluid h-50 w-50">
+        </a> -->
+        <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <i class="fas fa-bars"></i>
+        </button> -->
+
+        <a class="navbar-brand" href="#">
+            <img src="./assets/img/jawa-power.png" alt="Jawa Power" class="img-fluid h-75 w-75">
+            <!-- Navbar -->
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ">
                 <li class="nav-item custom-nav-item align-item-center align-self-center">
@@ -20,7 +29,7 @@
                 <li class="nav-item custom-nav-item align-item-center align-self-center">
                     <a class="nav-link text-dark-custom" href="./">Profile</a>
                 </li>
-                
+
             </ul>
         </div>
     </div>
